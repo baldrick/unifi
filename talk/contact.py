98238f660c312d510fec12c2fc672f233276e8b0
Gavin Manning <git@gavinmanning.com>
2026-01-30 17:18:49 +0000

    Add --grandstream to output XML files for upload to Grandstream phones

--- talk/contact.py
+++ talk/contact.py
@@ -8,4 +8,31 @@ class Contact:
         self.work_number = work_number
 
     def __str__(self):
+        return self.unifi_csv()
+
+    def unifi_csv(self):
         return f'"{self.first_name}","{self.last_name}",,,"{self.email}","{self.mobile_number}","{self.home_number}","{self.work_number}",,'
+
+    def grandstream_xml(self, id):
+        # Should use a library to generate this but it's not obvious which one...
+        # ... and our requirements are simple so this hack will do for now!
+        return f'''<Contact>
+            <id>{id}</id>
+            <FirstName>{self.first_name}</FirstName>
+            <LastName>{self.last_name}</LastName>
+            <RingtoneUrl>default ringtone</RingtoneUrl>
+            <Frequent>0</Frequent>
+            {self.grandstream_xml_number(1, "Home", self.home_number)}
+            {self.grandstream_xml_number(1, "Cell", self.mobile_number)}
+            {self.grandstream_xml_number(1, "Work", self.work_number)}
+            <Primary>0</Primary>
+        </Contact>'''
+
+    def grandstream_xml_number(self, account_index, label, number):
+        if len(number) == 0:
+            return ''
+        return f'''<Phone type="{label}">
+                    <phonenumber>{number}</phonenumber>
+                    <accountindex>{account_index}</accountindex>
+            </Phone>
+            '''
--- talk/sync/sync.py
+++ talk/sync/sync.py
@@ -11,7 +11,8 @@ HEADER = 'first_name,last_name,company,job_title,email,mobile_number,home_number
 def add_commands(subparsers):
     sync_parser = subparsers.add_parser('sync', help='sync contacts from Google to Unifi Talk')
     sync_parser.add_argument('--labels', nargs='+', help='contacts for these labels will be synced', type=str)
-    sync_parser.add_argument('--csv', action='store_const', const=True, help='output contacts to CSV files')
+    sync_parser.add_argument('--unifi_csv', action='store_const', const=True, help='output contacts to CSV file for Unifi')
+    sync_parser.add_argument('--grandstream', action='store_const', const=True, help='output contacts to XML file for Grandstream')
     sync_parser.add_argument('--unifi', action='store_const', const=True, help='sync contacts with Unifi Talk')
     sync_parser.set_defaults(func=sync_contacts)
 
@@ -22,45 +23,30 @@ def sync_contacts(args):
         return
 
     # Get contacts from Google, create map of contacts for given labels.
+    # Normalize those contacts by collating ones with the same home number.
     raw_contacts = GoogleContacts()
-    filtered_contacts = {}
-    for label in args.labels:
-        filtered_contacts[label] = raw_contacts.filter(label)
+    normalized_contacts = {label: normalize(raw_contacts.filter(label), label) for label in args.labels}
+    #for label in args.labels:
+    #    normalized_contacts[label] = normalize(raw_contacts.filter(label), label)
 
-    if not args.csv and not args.unifi:
-        logger.error('at least one of --csv or --unifi must be specified')
+    if not args.unifi_csv and not args.grandstream and not args.unifi:
+        logger.error('at least one of --csv, --grandstream or --unifi must be specified')
         return
 
-    # Output CSV files if requested.
-    if args.csv:
-        for label in args.labels:
-            filename = write_csv(filtered_contacts[label], label)
-            logger.info(f'wrote {filename}')
-
-    # Sync with Unifi Talk if requested.
-    if args.unifi:
-        api = TalkAPI(args.server, args.username, args.password)
-        ids, response = api.delete_all_contacts()
-        if response is not False:
-            logger.info(f'deleted all {len(ids)} contacts from Unifi Talk') if ids is not None else logger.info('no contacts to delete from Unifi Talk')
-            cl_map = api.get_contact_lists()
-            for label in args.labels:
-                api.save_contacts(label, filtered_contacts[label], cl_map[label]['id'])
-                logger.info(f'saved {len(filtered_contacts[label])} {label} contacts to Unifi Talk')
-        else:
-            logger.warning('failed to delete contacts from Unifi Talk: {deleted}')
+    write_unifi_csv(args, normalized_contacts)
+    write_grandstream_xml(args, normalized_contacts)
+    write_unifi(args, normalized_contacts)
 
 
-def write_csv(contacts, group_id):
-    logger.debug(f'{len(contacts)} {group_id} raw contacts found')
-    deduped_by_home_number, contacts_without_home_number = dedup_by_home_number(contacts)
+def normalize(contacts, group_id):
+    group_contacts = contacts[group_id]
+    logger.debug(f'{len(group_contacts)} {group_id} raw contacts found')
+    deduped_by_home_number, contacts_without_home_number = dedup_by_home_number(group_contacts)
     deduped_by_home_number = add_cohabitants(deduped_by_home_number)
     logger.debug(f'{len(deduped_by_home_number) + len(contacts_without_home_number)} normalized {group_id} contacts found')
-    filename = f'{group_id}.csv'
-    with open(filename, 'w') as f:
-        f.write(HEADER + '\n')
-        f.write(contacts_as_csv(deduped_by_home_number, contacts_without_home_number))
-    return filename
+    normalized_contacts = [c for c in dedup_by_home_number.items()]
+    normalized_contacts.extend(contacts_without_home_number)
+    return normalized_contacts
 
 
 def dedup_by_home_number(contacts):
@@ -104,14 +90,55 @@ def add_cohabitants(deduped_by_home_number):
     return deduped_by_home_number
 
 
-def contacts_as_csv(deduped_by_home_number, contacts_without_home_number) -> str:
-    output = ''
-    for hn, contacts in deduped_by_home_number.items():
-        if contacts is None:
-            logger.warning(f'contacts is None for {hn}')
-            continue
-        for contact in contacts:
-            output += contact.__str__() + '\n'
-    for contact in contacts_without_home_number:
-        output += contact.__str__() + '\n'
-    return output
+def write_unifi_csv(args, contacts):
+    if not args.unifi_csv:
+        return
+
+    for label in args.labels:
+        filename = f'{label}.csv'
+        with open(filename, 'w') as f:
+            f.write(HEADER + '\n')
+            f.write('\n'.join([f'{c.unifi_csv()}' for c in contacts]))
+        logger.info(f'wrote {filename}')
+
+
+def write_grandstream_xml(args, contacts):
+    if not args.grandstream:
+        return
+
+    for label in args.labels:
+        filename = f'{label}.csv'
+        with open(filename, 'w') as f:
+            f.write('<AddressBook>\n')
+            f.write('\n'.join([f'{c.grandstream_xml(i)}' for i, c in enumerate(contacts)]))
+            f.write('</AddressBook>')
+        logger.info(f'wrote {filename}')
+
+
+def write_unifi(args, contacts):
+    if not args.unifi:
+        return
+
+    api = TalkAPI(args.server, args.username, args.password)
+    ids, response = api.delete_all_contacts()
+    if response is not False:
+        logger.info(f'deleted all {len(ids)} contacts from Unifi Talk') if ids is not None else logger.info('no contacts to delete from Unifi Talk')
+        cl_map = api.get_contact_lists()
+        for label in args.labels:
+            api.save_contacts(label, contacts[label], cl_map[label]['id'])
+            logger.info(f'saved {len(contacts[label])} {label} contacts to Unifi Talk')
+    else:
+        logger.warning('failed to delete contacts from Unifi Talk: {deleted}')
+
+
+# def contacts_as_csv(contacts) -> str:
+#     return 
+    # for hn, contacts in deduped_by_home_number.items():
+    #     if contacts is None:
+    #         logger.warning(f'contacts is None for {hn}')
+    #         continue
+    #     for contact in contacts:
+    #         output += contact.__str__() + '\n'
+    # for contact in contacts_without_home_number:
+    #     output += contact.__str__() + '\n'
+    # return output
